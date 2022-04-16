26a4df56a8d0527cf9dc02adb31f5c55a34e0db6
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-04-16 20:06:09 +0200

    auth middleware added

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const validation = require("./validation");
 const controllsWrapper = require("./controllsWrapper");
+const auth = require("./auth");
 
-module.exports = { validation, controllsWrapper };
+module.exports = { validation, controllsWrapper, auth };
